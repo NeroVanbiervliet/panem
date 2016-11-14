64a9d154454c8974862e94015ed37264401044a6
Nero <louis.vanbiervliet@gmail.com>
2016-11-14 17:21:51 +0100

    back function fixed on register and log in buttons

--- src/static/frontend/js/controllers/navbar.js
+++ src/static/frontend/js/controllers/navbar.js
@@ -36,11 +36,17 @@ loadNavBarLogic = function(userInfo, $rootScope, $location, tokenManager, dictio
 
         // redirect to home
         $window.location = '#/client/home/';
-    }
+    };
 
     // EXECUTED ON ROUTE CHANGE AND ON REFRESH
     $rootScope.$on("$routeChangeSuccess", function($currentRoute, $previousRoute) {
         // used to redirect to the source page after successful log in on login page
-        $rootScope.urlPath = $location.absUrl().split("#")[1]
+        $rootScope.urlPath = $location.absUrl().split("#")[1];
     });
-}
+
+    // routing
+    // cannot use navigate(someUrl) when the url contains ?somevar, the back function will then be broken
+    $rootScope.changeUrl= function(targetUrl) {
+        $location.url(targetUrl);
+    };
+};
